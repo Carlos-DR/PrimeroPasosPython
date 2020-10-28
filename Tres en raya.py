16bb52756c76cf6605d35d2c212e712e78811e3f
Carlos-DR <carlosdominguezrastrojo@gmail.com>
2020-10-28 10:18:00 +0100

    Cosas que hago por aburriemiento y aprendizaje V.1.1
    Tres en raya para 2 jugadores

--- Tres en raya.py
+++ Tres en raya.py
@@ -1,139 +1,111 @@
-import random
+# Implementación del juego Tic-Tac-Toe para dos jugadores en Python.
 
-print('Tres en raya')
-print('Elige con que quieres jugar X o O')
+'''' Haremos el tablero usando diccionario.
+     en qué teclas estará la ubicación (es decir, arriba a la izquierda, a la mitad a la derecha, etc.)
+     e inicialmente sus valores serán un espacio vacío y luego, después de cada movimiento
+     cambiaremos el valor de acuerdo con la elección de movimiento del jugador. '''
 
-tablero = {'7': ' ', '8': ' ', '9': ' ', '4': ' ', '5': ' ', '6': ' ', '1': ' ', '2': ' ', '3': ' '}
+theBoard = {'7': ' ', '8': ' ', '9': ' ',
+            '4': ' ', '5': ' ', '6': ' ',
+            '1': ' ', '2': ' ', '3': ' '}
 
-tictactoc1 = {'7': ' ', '8': ' ', '9': ' ', '4': ' ', '5': ' ', '6': ' ', '1': 'X', '2': 'X', '3': 'X'}
-tictactoc2 = {'7': ' ', '8': ' ', '9': ' ', '4': 'X', '5': 'X', '6': 'X', '1': ' ', '2': ' ', '3': ' '}
-tictactoc3 = {'7': 'X', '8': 'X', '9': 'X', '4': ' ', '5': ' ', '6': ' ', '1': ' ', '2': ' ', '3': ' '}
-tictactoc4 = {'7': 'X', '8': ' ', '9': ' ', '4': 'X', '5': ' ', '6': ' ', '1': 'X', '2': ' ', '3': ' '}
-tictactoc5 = {'7': ' ', '8': 'X', '9': ' ', '4': ' ', '5': 'X', '6': ' ', '1': ' ', '2': 'X', '3': ' '}
-tictactoc6 = {'7': ' ', '8': ' ', '9': 'X', '4': ' ', '5': ' ', '6': 'X', '1': ' ', '2': ' ', '3': 'X'}
-tictactoc7 = {'7': ' ', '8': ' ', '9': 'X', '4': ' ', '5': 'X', '6': ' ', '1': 'X', '2': ' ', '3': ' '}
-tictactoc8 = {'7': 'X', '8': ' ', '9': ' ', '4': ' ', '5': 'X', '6': ' ', '1': ' ', '2': ' ', '3': 'X'}
+board_keys = []
 
-tictactoc9 = {'7': ' ', '8': ' ', '9': ' ', '4': ' ', '5': ' ', '6': ' ', '1': 'O', '2': 'O', '3': 'O'}
-tictactoc10 = {'7': ' ', '8': ' ', '9': ' ', '4': 'O', '5': 'O', '6': 'O', '1': ' ', '2': ' ', '3': ' '}
-tictactoc11 = {'7': 'O', '8': 'O', '9': 'O', '4': ' ', '5': ' ', '6': ' ', '1': ' ', '2': ' ', '3': ' '}
-tictactoc12 = {'7': 'O', '8': ' ', '9': ' ', '4': 'O', '5': ' ', '6': ' ', '1': 'O', '2': ' ', '3': ' '}
-tictactoc13 = {'7': ' ', '8': 'O', '9': ' ', '4': ' ', '5': 'O', '6': ' ', '1': ' ', '2': 'O', '3': ' '}
-tictactoc14 = {'7': ' ', '8': ' ', '9': 'O', '4': ' ', '5': ' ', '6': 'O', '1': ' ', '2': ' ', '3': 'O'}
-tictactoc15 = {'7': ' ', '8': ' ', '9': 'O', '4': ' ', '5': 'O', '6': ' ', '1': 'O', '2': ' ', '3': ' '}
-tictactoc16 = {'7': 'O', '8': ' ', '9': ' ', '4': ' ', '5': 'O', '6': ' ', '1': ' ', '2': ' ', '3': 'O'}
+for key in theBoard:
+    board_keys.append(key)
 
-jugador = input()
-jugador = jugador.upper()
-bot = 'A'
-
-fin = 0
-win = 0
-array = []
-
-while fin == 0:
-
-    if jugador == 'X':
-        bot = 'O'
-        fin = 1
-    if jugador == 'O':
-        bot = 'X'
-        fin = 1
-    elif jugador != 'X' and jugador != 'O':
-        print('Por favor, introduzca un valor válido (X o O)')
-        jugador = input()
-        jugador = jugador.upper()
+''' Tendremos que imprimir el tablero actualizado después de cada movimiento en el juego y
+     así haremos una función en la que definiremos la función printBoard
+     para que podamos imprimir fácilmente el tablero cada vez llamando a esta función. '''
 
 
-def imprimirtablero(tab):
-    print('+---+---+---+')
-    print('| ' + tab['7'] + ' | ' + tab['8'] + ' | ' + tab['9'] + ' |')
-    print('+---+---+---+')
-    print('| ' + tab['4'] + ' | ' + tab['5'] + ' | ' + tab['6'] + ' |')
-    print('+---+---+---+')
-    print('| ' + tab['1'] + ' | ' + tab['2'] + ' | ' + tab['3'] + ' |')
-    print('+---+---+---+')
+def printBoard(board):
+    print(board['7'] + '|' + board['8'] + '|' + board['9'])
+    print('-+-+-')
+    print(board['4'] + '|' + board['5'] + '|' + board['6'])
+    print('-+-+-')
+    print(board['1'] + '|' + board['2'] + '|' + board['3'])
 
 
-def comprobarposicion():
-    # Posición del X
-    if tablero == variable:
-        return True
-    if tablero == tictactoc2:
-        return True
-    if tablero == tictactoc3:
-        return True
-    if tablero == tictactoc4:
-        return True
-    if tablero == tictactoc5:
-        return True
-    if tablero == tictactoc6:
-        return True
-    if tablero == tictactoc7:
-        return True
-    if tablero == tictactoc8:
-        return True
+# Ahora escribiremos la función principal que tiene toda la funcionalidad del juego..
+def game():
+    turn = 'X'
+    count = 0
 
-    # Posición del O
-    if tablero == tictactoc9:
-        return True
-    if tablero == tictactoc10:
-        return True
-    if tablero == tictactoc11:
-        return True
-    if tablero == tictactoc12:
-        return True
-    if tablero == tictactoc13:
-        return True
-    if tablero == tictactoc14:
-        return True
-    if tablero == tictactoc15:
-        return True
-    if tablero == tictactoc16:
-        return True
-    else:
-        return False
+    for i in range(10):
+        printBoard(theBoard)
+        print("Es tu turno," + turn + ".¿Donde quieres marcar?")
+
+        move = input()
+
+        if theBoard[move] == ' ':
+            theBoard[move] = turn
+            count += 1
+        else:
+            print("Ya hay una marca en ese lugar.\n¿Donde quieres marcar?")
+            continue
+
+        # Ahora comprobaremos si el jugador X u O ha ganado, por cada movimiento después de 5 movimientos..
+        if count >= 5:
+            if theBoard['7'] == theBoard['8'] == theBoard['9'] != ' ':  # tres en raya horizontal arriba
+                printBoard(theBoard)
+                print("\nFin del juego.\n")
+                print(" **** " + turn + " gana. ****")
+                break
+            elif theBoard['4'] == theBoard['5'] == theBoard['6'] != ' ':  # tres en raya horizontal en medio
+                printBoard(theBoard)
+                print("\nFin del juego.\n")
+                print(" **** " + turn + " gana. ****")
+                break
+            elif theBoard['1'] == theBoard['2'] == theBoard['3'] != ' ':  # tres en raya horizontal abajo
+                printBoard(theBoard)
+                print("\nFin del juego.\n")
+                print(" **** " + turn + " gana. ****")
+                break
+            elif theBoard['1'] == theBoard['4'] == theBoard['7'] != ' ':  # tres en raya vertical izquierdo
+                printBoard(theBoard)
+                print("\nFin del juego.\n")
+                print(" **** " + turn + " gana. ****")
+                break
+            elif theBoard['2'] == theBoard['5'] == theBoard['8'] != ' ':  # tres en raya vertical en medio
+                printBoard(theBoard)
+                print("\nFin del juego.\n")
+                print(" **** " + turn + " gana. ****")
+                break
+            elif theBoard['3'] == theBoard['6'] == theBoard['9'] != ' ':  # tres en raya vertical derecho
+                printBoard(theBoard)
+                print("\nFin del juego.\n")
+                print(" **** " + turn + " gana. ****")
+                break
+            elif theBoard['7'] == theBoard['5'] == theBoard['3'] != ' ':  # diagonal 1
+                printBoard(theBoard)
+                print("\nFin del juego.\n")
+                print(" **** " + turn + " gana. ****")
+                break
+            elif theBoard['1'] == theBoard['5'] == theBoard['9'] != ' ':  # diagonal 2
+                printBoard(theBoard)
+                print("\nFin del juego.\n")
+                print(" **** " + turn + " gana. ****")
+                break
+
+                # Si ni X ni O ganan y el tablero está lleno, declararemos el resultado como 'empate'.
+        if count == 9:
+            print("\nFin del juego.\n")
+            print("EMPATE!!")
+
+        # Cambiamos de jugador después de cada movimiento.
+        if turn == 'X':
+            turn = 'O'
+        else:
+            turn = 'X'
+
+            # Preguntamos si quieres reiniciar el juego o no.
+    restart = input("¿Quieres jugar otra vez?(s/n)")
+    if restart == "s" or restart == "S":
+        for key in board_keys:
+            theBoard[key] = " "
+        game()
 
 
-while not comprobarposicion():
-    print('¿Donde quieres marcar?')
-    imprimirtablero(tablero)
-    print(tablero)
-    marca = input()
-
-    if marca in array:
-        print('Por favor, marque en una casilla en blanco')
-
-    if marca not in array:
-        array.append(marca)
-        tablero[marca] = jugador
-        numbot = str(random.randint(1, 9))
-        print(numbot)
-
-        if len(array) != 9:
-            bucle = 1
-
-            while bucle == 1:
-                
-                if numbot in array:
-                    numbot = str(random.randint(1, 9))
-                    print('nuevo numbot', numbot)
-
-                if numbot not in array:
-                    array.append(numbot)
-                    tablero[numbot] = bot
-                    imprimirtablero(tablero)
-                    bucle = 0
-
-                else:
-                    print('Ha dado un caske')
-
-        elif len(array) == 9:
-            imprimirtablero(tablero)
-            print('Fin')
-            comprobarposicion()
-
-    elif len(array) == 9:
-        imprimirtablero(tablero)
-        print('Fin')
-        comprobarposicion()
+if __name__ == "__main__":
+    game()
--- main.py
+++ main.py
@@ -7,7 +7,7 @@ while salir != 1:
     print(' ')
     print('1. Adivina los dados')
     print('2. Piedra, papel y tijera')
-    print('3. Tres en raya')
+    print('3. Tres en raya (2 jugadores)')
     print('4. Snake')
     print('5. Buscaminas')
     print('0. SALIR')
@@ -44,7 +44,7 @@ while salir != 1:
 
         win = 5
         while win > 1:
-            print('INTENTOS RESTANTES:', win - 1)
+            print('INTENTOS RESTANTES:', win - 1, '\n')
             if suma == numero:
                 print('Enhorabuena, has acertado :D')
                 win = 0
@@ -103,7 +103,117 @@ while salir != 1:
     if opcion == 3:
         print('TRES EN RAYA')
         print(' ')
-        print('EN DESARROLLO')
+        # Implementación del juego Tic-Tac-Toe para dos jugadores en Python.
+
+        '''' Haremos el tablero usando diccionario.
+             en qué teclas estará la ubicación (es decir, arriba a la izquierda, a la mitad a la derecha, etc.)
+             e inicialmente sus valores serán un espacio vacío y luego, después de cada movimiento
+             cambiaremos el valor de acuerdo con la elección de movimiento del jugador. '''
+
+        theBoard = {'7': ' ', '8': ' ', '9': ' ',
+                    '4': ' ', '5': ' ', '6': ' ',
+                    '1': ' ', '2': ' ', '3': ' '}
+
+        board_keys = []
+
+        for key in theBoard:
+            board_keys.append(key)
+
+        ''' Tendremos que imprimir el tablero actualizado después de cada movimiento en el juego y
+             así haremos una función en la que definiremos la función printBoard
+             para que podamos imprimir fácilmente el tablero cada vez llamando a esta función. '''
+
+
+        def printBoard(board):
+            print(board['7'] + '|' + board['8'] + '|' + board['9'])
+            print('-+-+-')
+            print(board['4'] + '|' + board['5'] + '|' + board['6'])
+            print('-+-+-')
+            print(board['1'] + '|' + board['2'] + '|' + board['3'])
+
+
+        # Ahora escribiremos la función principal que tiene toda la funcionalidad del juego..
+        def game():
+            turn = 'X'
+            count = 0
+
+            for i in range(10):
+                printBoard(theBoard)
+                print("Es tu turno," + turn + ".¿Donde quieres marcar?")
+
+                move = input()
+
+                if theBoard[move] == ' ':
+                    theBoard[move] = turn
+                    count += 1
+                else:
+                    print("Ya hay una marca en ese lugar.\n¿Donde quieres marcar?")
+                    continue
+
+                # Ahora comprobaremos si el jugador X u O ha ganado, por cada movimiento después de 5 movimientos..
+                if count >= 5:
+                    if theBoard['7'] == theBoard['8'] == theBoard['9'] != ' ':  # tres en raya horizontal arriba
+                        printBoard(theBoard)
+                        print("\nFin del juego.\n")
+                        print(" **** " + turn + " gana. ****")
+                        break
+                    elif theBoard['4'] == theBoard['5'] == theBoard['6'] != ' ':  # tres en raya horizontal en medio
+                        printBoard(theBoard)
+                        print("\nFin del juego.\n")
+                        print(" **** " + turn + " gana. ****")
+                        break
+                    elif theBoard['1'] == theBoard['2'] == theBoard['3'] != ' ':  # tres en raya horizontal abajo
+                        printBoard(theBoard)
+                        print("\nFin del juego.\n")
+                        print(" **** " + turn + " gana. ****")
+                        break
+                    elif theBoard['1'] == theBoard['4'] == theBoard['7'] != ' ':  # tres en raya vertical izquierdo
+                        printBoard(theBoard)
+                        print("\nFin del juego.\n")
+                        print(" **** " + turn + " gana. ****")
+                        break
+                    elif theBoard['2'] == theBoard['5'] == theBoard['8'] != ' ':  # tres en raya vertical en medio
+                        printBoard(theBoard)
+                        print("\nFin del juego.\n")
+                        print(" **** " + turn + " gana. ****")
+                        break
+                    elif theBoard['3'] == theBoard['6'] == theBoard['9'] != ' ':  # tres en raya vertical derecho
+                        printBoard(theBoard)
+                        print("\nFin del juego.\n")
+                        print(" **** " + turn + " gana. ****")
+                        break
+                    elif theBoard['7'] == theBoard['5'] == theBoard['3'] != ' ':  # diagonal 1
+                        printBoard(theBoard)
+                        print("\nFin del juego.\n")
+                        print(" **** " + turn + " gana. ****")
+                        break
+                    elif theBoard['1'] == theBoard['5'] == theBoard['9'] != ' ':  # diagonal 2
+                        printBoard(theBoard)
+                        print("\nFin del juego.\n")
+                        print(" **** " + turn + " gana. ****")
+                        break
+
+                        # Si ni X ni O ganan y el tablero está lleno, declararemos el resultado como 'empate'.
+                if count == 9:
+                    print("\nFin del juego.\n")
+                    print("EMPATE!!")
+
+                # Cambiamos de jugador después de cada movimiento.
+                if turn == 'X':
+                    turn = 'O'
+                else:
+                    turn = 'X'
+
+                    # Preguntamos si quieres reiniciar el juego o no.
+            restart = input("¿Quieres jugar otra vez?(s/n)")
+            if restart == "s" or restart == "S":
+                for key in board_keys:
+                    theBoard[key] = " "
+                game()
+
+
+        if __name__ == "__main__":
+            game()
 
     if opcion == 4:
         print('SNAKE')
